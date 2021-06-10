9e94d78f7cce5840dfd8bf8f1b2953766924a439
SupitS <84420067+SupitS@users.noreply.github.com>
2021-06-10 23:08:06 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+// Opdracht 017: Function basics
+
+console.log('Hallo Winc Academy studenten');
+
+// Define a function
+const makePizza = function () {
+    console.log('Make pizza dough');
+    console.log('Add tomato sauce on top of the dough');
+    console.log('Add cheese, mushrooms and sausages');
+    console.log('Put in a pre-heated oven');
+};
+
+
+const makeSushi = function () {
+    console.log('Make rice');
+    console.log('Cut salmon in pieces');
+    console.log('Combine rice and salmon together');
+    console.log('Put as sidedish wasabi and soy sauce');
+};
+
+// 'calling' a function
+makeSushi();
+makeSushi();
+makeSushi();
+makePizza();
+makePizza();
